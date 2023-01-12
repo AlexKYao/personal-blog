9a09589bf3c30cf3dfa0ceae1997edc49ade2816
Alexander Yao <9095664+AlexKYao@users.noreply.github.com>
2023-01-12 14:24:35 -0600

    Updating content

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Alex Yao';
+export const SITE_DESCRIPTION = 'My name is Alex Yao and I live in Mexico City. I am currently working on Dialecta. Contact me by DM on Twitter @TheAlexYao.';
